8f8159fac16eb83f9f670bb7cc17aa800727c297
fab2s35 <sosorto72@gmail.com>
2025-07-09 09:49:13 -0600

    documentacion

--- backend/app.js
+++ backend/app.js
@@ -3,13 +3,25 @@ import express from "express";
 import clientsRoutes from "./src/routes/clients.js";
 import appointmentRoutes from "./src/routes/appointment.js"
 
+import swaggerUi from "swagger-ui-express";
+import fs from "fs";
+import path from "path";
+
 // Creo una constante que es igual a la libreria que importé
 const app = express();
 
 //Que acepte datos en json
 app.use(express.json());
 
+const swaggerDocument = JSON.parse(
+  fs.readFileSync(
+    path.resolve("./documentation.json"),
+    "utf-8"
+  )
+)
+
 // Definir las rutas de las funciones que tendr á la página web
+app.use("/api/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use("/api/clients", clientsRoutes);
 app.use("/api/appointment", appointmentRoutes);
